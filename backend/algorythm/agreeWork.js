cf92a3add313ad84b6f3e438221f26cb59da365a
t-a1024 <133000386+t-a1024@users.noreply.github.com>
2025-04-27 21:39:43 +0900

    コメント修正

--- backend/algorythm/agreeWork.js
+++ backend/algorythm/agreeWork.js
@@ -1,7 +1,9 @@
 function agreeWorkBuildByNPO(params) {
     // ホームレスが働くことに問題ないことを確認を受け取った際の処理
+    // contactNPOForWorkBuildを呼び出す
 }
 
-function WorkBuildByOwner(params) {
+function WorkBuildByOwner(params) {// 引数として、空き家のidとホームレスのidを受け取る
     // ホームレスが働くことに問題ないことを確認を受け取った際の処理
+    // dbに書き加える
 }
