e0429685110a4f6390d405a1943451d2b9e6493a
t-a1024 <133000386+t-a1024@users.noreply.github.com>
2025-04-27 23:39:34 +0900

    サーバーから情報を取得する例

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5001;
+// CORS設定（すべてのオリジンからのアクセスを許可）
+app.use(cors({
+    origin: '*', // '*' で全てのオリジンからアクセスを許可
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  }));
 
 // Middleware to parse JSON bodies
 app.use(express.json());
--- frontend1/src/App.jsx
+++ frontend1/src/App.jsx
@@ -7,6 +7,14 @@ function App() {
   return (
     <>
       <h1>This is page 1!!</h1>
+      <button onClick={() => {
+        fetch('http://0.0.0.0:5001/api')
+          .then(response => response.text())
+          .then(data => {alert(data);console.log(data)})
+          .catch(error => console.error('Error:', error));
+      }}>
+        Fetch from Port 5000
+      </button>
     </>
   )
 }
