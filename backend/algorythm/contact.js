6c313bbe3a11a28470cc5c52c328421f7cc215a5
t-a1024 <133000386+t-a1024@users.noreply.github.com>
2025-04-28 15:34:54 +0900

    メール機能の実装

--- backend/algorythm/contact.js
+++ backend/algorythm/contact.js
@@ -1,3 +1,5 @@
+const nodemailer = require('nodemailer');
+
 function contactOwner(params) { //引数(ユーザーid,メッセージ)
     // オーナーと連絡を取る
 }
@@ -13,6 +15,7 @@ function contactOwnerForWorkBuild(params) {
     //     mattchWorksearcher:jobSercherId(int)[]
     //     }
     // メールを送信
+    sendMail()
 }
 
 function contactNPOForWorkBuild(params) {
@@ -26,4 +29,29 @@ function contactNPOForWorkBuild(params) {
 
 function notificationDeleteBuildDate(buildId) {
     // 建物が削除された通知
+}
+
+async function sendMail(to, subject, message) {
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            // あなたのメールアドレス TODO .envの実装
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+        }
+    });
+
+    const mailOptions = {
+        from: 'your-email@gmail.com',
+        to: to,
+        subject: subject,
+        text: message
+    };
+
+    try {
+        const info = await transporter.sendMail(mailOptions);
+        console.log('Email sent: ' + info.response);
+    } catch (error) {
+        console.error('Error sending email: ', error);
+    }
 }
